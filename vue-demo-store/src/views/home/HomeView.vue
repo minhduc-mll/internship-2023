3211fb050dce2392612c5ebda95ba8855f506e73
MinhDuc <minhduc.mll@gmail.com>
2023-06-23 11:16:06 +0700

    Format

--- vue-demo-store/src/views/home/HomeView.vue
+++ vue-demo-store/src/views/home/HomeView.vue
@@ -1,16 +1,8 @@
 <template>
-  <BaseLayout>
-    <UnitTestComponent msg="Hello world" />
-    <EventBus msg="Event Bus" />
-    <button @click="app.onClickA">Change to A</button>
-    <button @click="app.onClickB">Change to B</button>
-    <button @click="app.onClickC">Event Bus</button>
-  </BaseLayout>
+  <BaseLayout> </BaseLayout>
 </template>
 
 <script setup lang="ts">
-import EventBus from "@/components/EventBus.vue";
-import UnitTestComponent from "@/components/UnitTestComponent.vue";
 import BaseLayout from "@/layouts/BaseLayout.vue";
 import { BaseComponent, defineClassComponent } from "@/plugins/component.plugin";
 
@@ -18,23 +10,7 @@ const app = defineClassComponent(
   class Component extends BaseComponent {
     public constructor() {
       super();
-
-      this.searchParams.onStateChange((params) => {
-        console.log("Hehe from Home", params);
-      });
     }
-
-    public onClickA = () => {
-      this.searchParams.changeParams({ a: "thang", b: "" });
-    };
-
-    public onClickB = () => {
-      this.searchParams.changeParams({ a: "", b: "thang" });
-    };
-
-    public onClickC = () => {
-      this.commonStore.eventBus.emit("click", { hello: "world" });
-    };
   },
 );
 </script>
